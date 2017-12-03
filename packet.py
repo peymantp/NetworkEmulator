5236561d1839447cd3744559a635dada43536531
Peyman Tehrani Parsa <peymantparsa@gmail.com>
2017-12-03 04:36:02 -0800

    completed packet class

--- packet.py
+++ packet.py
@@ -1,9 +1,44 @@
 #! python3
 import hashlib
 class packet:
-    def __init__(self, packetType, seqNum, windowSize, ackNum, data=""):
-        self.packetType = packetType
-        self.seqNum     = seqNum
-        self.windowSize = windowSize
-        self.ackNum     = ackNum
-        checksum = data #TODO implement sha1
+    '''
+    This module keeps track of the packets sent for one transmission
+
+    packetType type:int,
+        0 = Data,
+        1 = ACK,
+        2 = SOT,
+        3 = EOT
+    '''
+    delim = "|:|"
+    packetType = 0
+    seqNum = 0
+    windowSize = 0
+    ackNum = 0
+    data = ""
+    def create(packetType, SeqNum, WindowSize, AckNum, Data=""):
+        packetType = packetType
+        seqNum     = SeqNum
+        windowSize = WindowSize
+        ackNum     = AckNum
+        data = Data #TODO implement sha1
+
+    def toString():
+        stringPacket = (str(packet.packetType) +packet.delim
+                        +str(packet.seqNum)    +packet.delim
+                        +str(packet.windowSize)+packet.delim
+                        +str(packet.ackNum)    +packet.delim
+                        +packet.data)
+        return stringPacket
+    
+    @staticmethod
+    def parse(p):
+        '''
+        array[0] = Packet type\n
+        array[1] = sequence number\n
+        array[2] = Window size\n
+        array[3] = ack number\n
+        array[4] = data (if data packet)
+        '''
+        packetArray = p.split(packet.delim)
+        return packetArray
